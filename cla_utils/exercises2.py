Working implementation of householder - mistake was to change A in place before the final calculation

cla_utils/exercises2.py
@@ -105,7 +105,7 @@ def GS_classical(A):
     """
 
     n = np.size(A, 1)
-    R = np.zeros((n, n), dtype=complex)
+    R = np.zeros((n, n), dtype=A.dtype)
 
     for j in range(n):
         R[:j, j] = A[:, :j].conj().T @ A[:, j]
@@ -128,7 +128,7 @@ def GS_modified(A):
     """
 
     n = np.size(A, 1)
-    R = np.zeros((n, n), dtype=complex)
+    R = np.zeros((n, n), dtype=A.dtype)
 
     for i in range(n):
         R[i, i] = np.linalg.norm(A[:, i])
@@ -138,6 +138,17 @@ def GS_modified(A):
             A[:, j] -= R[i, j] * A[:, i]
 
     return R
+
+# Ex 3.6
+
+
+def mutual_orthogonality():
+    A = np.random.rand(100, 100)
+    A_copy = np.copy(A)
+    R_classical = GS_classical(A)
+    R_modified = GS_modified(A_copy)
+
+    return A @ A_copy
 
 
 def GS_modified_get_R(A, k):
@@ -154,9 +165,14 @@ def GS_modified_get_R(A, k):
     :return R: nxn numpy array
     """
 
-    raise NotImplementedError
+    n = np.size(A, 1)
+    R = np.identity(n, dtype=A.dtype)
+
+    R[k, k] = np.linalg.norm(A[:, k])
+    R[k, k+1:] = A[:, k] @ A[:, k+1:]
 
     return R
+
 
 def GS_modified_R(A):
     """

cla_utils/exercises3.py
@@ -16,7 +16,20 @@ def householder(A, kmax=None):
     if kmax is None:
         kmax = n
 
-    raise NotImplementedError
+    for k in range(kmax):
+        x = np.copy(A[k:, k])
+        if np.sign(x[0]) == 0:
+            x[0] += np.linalg.norm(x)
+        else:
+            x[0] += np.sign(x[0]) * np.linalg.norm(x)
+
+        x /= np.linalg.norm(x)
+        # inner = np.inner(x.conj(), A[k:, k:].T)  # This produces the wrong output
+        # outer = np.outer(x, inner)
+        # A[k:, k:] -= 2 * outer
+        A[k:, k:] -= 2 * np.outer(x, np.inner(x.conj(), A[k:, k:].T))
+
+    # return A
 
 
 def solve_U(U, b):

test/test_exercises2.py
@@ -92,7 +92,7 @@ def test_GS_modified(m, n):
     A = random.randn(m, m) + 1j*random.randn(m, m)
     A = A[:, 1:n]
     A0 = 1.0*A
-    
+
     R = cla_utils.GS_modified(A)
 
     err = A0 - np.dot(A, R)